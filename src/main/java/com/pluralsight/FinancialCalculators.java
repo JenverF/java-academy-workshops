f8700bf93193173a961c94c2e497964ecb991794
JenverF <jenver.dfernandez@gmail.com>
2025-10-03 10:02:19 -0700

    Initial Commit

---
+++ src/main/java/com/pluralsight/FinancialCalculators.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class FinancialCalculators {
+    static void main() {
+        // Mortgage Calc
+        double principal = 53000; // P
+        double interestRate = 7.625; // r
+        double monthlyInterestRate = interestRate / 100 / 12;
+        int loanLength = 15;
+        int loanLengthInMonths = loanLength * 12;
+
+        double monthlyPayment = principal * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, loanLengthInMonths)) / ((Math.pow(1 + monthlyInterestRate, loanLengthInMonths)) - 1);
+
+        double totalInterestPaid = monthlyPayment * loanLengthInMonths - principal;
+        System.out.println("Monthly Payment: " + monthlyPayment);
+        System.out.println("Total interest paid: " + totalInterestPaid);
+
+        // Compound Interest Calc
+        double principal2 = 1000;
+        double interestRate2 = 1.75/100;
+        int nrOfYears = 5;
+
+        double futureValue = principal2 * Math.pow(1 + interestRate2 / 365, 365 * nrOfYears);
+        System.out.println(futureValue);
+
+        // Present Value Calc
+        double monthlyPayment2 = 3000;
+        int nrOfYears2 = 20;
+        int nrOfMonths = nrOfYears2 * 12;
+        double monthlyInterest = 2.5 / 100 / 12;
+
+        double presentValue = monthlyPayment2 * (1 - (1/Math.pow(1 + monthlyInterest, nrOfMonths))) / monthlyInterest;
+        System.out.println(presentValue);
+    }
+}
